98a092f183e1e201f908ffb191b779dff9718f6c
Leeveo <101552154+leeveo@users.noreply.github.com>
2025-07-27 09:20:04 +0200

    Update ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -4,10 +4,16 @@ module.exports = {
       name: "r3f-backend",
       script: "./index.js",
       interpreter: "node",
-   env: {
+      env: {
         NODE_ENV: "production",
         PORT: 3000
       }
+    },
+    {
+      name: "ngrok-tunnel",
+      script: "./ngrok-service.js",
+      interpreter: "node",
+      watch: false
     }
   ]
 };
